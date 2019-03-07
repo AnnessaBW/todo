3c252ba737a44a8591bd531a2947256197ef8da8
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-03-07 03:44:51 +0000

    add support for creating a new task through a RESTful JSON API

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,25 +5,30 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(4)
-      response_ids = response_value.collect do |task|
-        task["id"]
-      end
-      expect(response_ids).to eq([1, 2, task1.id, task2.id])
     end
   end
 
-  describe "task#update" do
-    it " should allow tasks to be marked as done" do
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task, done: false)
-      put :update, params: {id: task.id, task: { done: true}}
+      put :update, params: {id: task.id, task: { done: true }}
       expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
+    end
+  end
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
     end
   end
 end
