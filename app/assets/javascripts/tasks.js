12b3ff07932c7764807ed8b3da1d4d980303a39b
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-03-08 01:40:41 +0000

    adding strikethru

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,14 +5,16 @@
     // The taskHtml method takes in a JavaScript representation
     // of the task and produces an HTML representation using
     // <li> tags
-    function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-         task.title +
-         '</label></div></li>';
+  function taskHtml(task) {
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
 
       return liElement;
     }
@@ -31,7 +33,12 @@
         task: {
           done: doneValue
         }
-      });
+       }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+    } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -60,6 +67,7 @@
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
 
